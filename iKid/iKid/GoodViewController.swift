6005fcea2ecbc43b3fe5ea2be8935ee046bc2c2a
studentuser <studentuser@studentusers-MacBook-Pro.local>
2016-11-06 01:18:50 -0800

    finishing

--- iKid/iKid/GoodViewController.swift
+++ iKid/iKid/GoodViewController.swift
@@ -9,7 +9,55 @@
 import UIKit
 
 class GoodViewController: UIViewController {
-
+    private var GoodQViewController : GoodQViewController!
+    private var GoodAViewController : GoodAViewController!
+    
+    private func firstBuilder() {
+        if GoodQViewController == nil {
+            GoodQViewController = storyboard?.instantiateViewController(withIdentifier: "GoodQ") as! GoodQViewController
+        }
+    }
+    
+    private func secondBuilder() {
+        if GoodAViewController == nil {
+            GoodAViewController = storyboard?.instantiateViewController(withIdentifier: "GoodA") as! GoodAViewController
+        }
+    }
+    
+    @IBAction func flipBtn(_ sender: UIButton) {
+        secondBuilder()
+        firstBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        
+        if GoodQViewController != nil && GoodQViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            GoodAViewController.view.frame = view.frame
+            switchView(from: GoodQViewController, to: GoodAViewController)
+        } else {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            GoodQViewController.view.frame = view.frame
+            switchView(from: GoodAViewController, to: GoodQViewController)
+        }
+        UIView.commitAnimations()
+    }
+    
+    private func switchView(from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMove(toParentViewController: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParentViewController: self)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
